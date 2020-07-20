fef5c247a688f64fd6334f77936e78aa59a7b6f6
Helder <helder_cepeda@hotmail.com>
2020-07-20 14:38:24 +0100

    New commit

--- assets.py
+++ assets.py
@@ -7,15 +7,3 @@ import pandas as pd
 import requests
 
 
-class Assets:
-    def __init__(self):
-        self.api_key = API_KEY
-
-    def list(self):
-        url = r"https://api.cryptowat.ch/assets"
-        params = {
-            "apikey": self.api_key
-        }
-        result = requests.get(url, params=params).json()
-        df = pd.DataFrame(result["result"])
-        return df
