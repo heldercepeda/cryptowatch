02b27e5733e5794ab06f4338d122554aef91a304
Helder <helder_cepeda@hotmail.com>
2020-07-20 15:02:52 +0100

    PEP8

--- exchanges.py
+++ exchanges.py
@@ -6,25 +6,25 @@ import pandas as pd
 import requests
 
 
-#####################################################################################################
-### Exchanges are where all the action happens! Each exchange in our system has a list of markets ###
-#####################################################################################################
+#################################################################################################
+# Exchanges are where all the action happens! Each exchange in our system has a list of markets #
+#################################################################################################
 
 class Exchanges:
 
     def __init__(self):
         self.api_key = API_KEY
 
-    #####################################################################
-    ### This endpoint returns all exchanges in the Cryptowatch system ###
-    #####################################################################
+    #################################################################
+    # This endpoint returns all exchanges in the Cryptowatch system #
+    #################################################################
 
-    ### Returns a pandas dataframe
-    ### id         int64
-    ### symbol    object
-    ### name      object
-    ### route     object
-    ### active      bool
+    # Returns a pandas dataframe
+    # id         int64
+    # symbol    object
+    # name      object
+    # route     object
+    # active      bool
 
     def list(self):
         url = r"https://api.cryptowat.ch/exchanges"
@@ -35,17 +35,17 @@ class Exchanges:
         df = pd.DataFrame(result["result"])
         return df
 
-    ############################################################
-    ### This endpoint returns details for the given exchange ###
-    ############################################################
+    ########################################################
+    # This endpoint returns details for the given exchange #
+    ########################################################
 
-    ### Returns a dictionary with the following keys
-    ### id
-    ### symbol
-    ### name
-    ### active
-    ### routes
-    ###     markets
+    # Returns a dictionary with the following keys
+    # id
+    # symbol
+    # name
+    # active
+    # routes
+    #     markets
 
     def details(self, market):
         url = fr"https://api.cryptowat.ch/exchanges/{market}"
@@ -55,16 +55,16 @@ class Exchanges:
         result = requests.get(url, params=params).json()
         return result["result"]
 
-    #########################################################################
-    ### This endpoint returns all markets available on the given exchange ###
-    #########################################################################
+    #####################################################################
+    # This endpoint returns all markets available on the given exchange #
+    #####################################################################
 
-    ### Returns a pandas dataframe
-    ### id           int64
-    ### exchange    object
-    ### pair        object
-    ### active        bool
-    ### route       object
+    # Returns a pandas dataframe
+    # id           int64
+    # exchange    object
+    # pair        object
+    # active        bool
+    # route       object
 
     def markets(self, market):
         url = fr"https://api.cryptowat.ch/markets/{market}"
